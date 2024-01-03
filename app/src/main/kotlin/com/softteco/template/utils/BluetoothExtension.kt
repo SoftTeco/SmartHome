168472b0a96c8568a007478dc4064f2d4b9c358d
AlexJustUser <swimmersuper16@gmail.com>
2024-01-03 15:44:42 +0300

    fix: fix pr comments

--- app/src/main/kotlin/com/softteco/template/utils/BluetoothExtension.kt
+++ app/src/main/kotlin/com/softteco/template/utils/BluetoothExtension.kt
@@ -1,9 +1,6 @@
 package com.softteco.template.utils
 
-import android.graphics.Color
-import com.softteco.template.Constants
 import com.softteco.template.Constants.BIT_SHIFT_VALUE
-import java.util.Random
 
 @OptIn(ExperimentalUnsignedTypes::class)
 fun characteristicByteConversation(bytes: ByteArray, startIndex: Int, endIndex: Int): Double {
@@ -13,14 +10,4 @@ fun characteristicByteConversation(bytes: ByteArray, startIndex: Int, endIndex: 
         result = result or (array[i].toInt() shl BIT_SHIFT_VALUE * i)
     }
     return result.toDouble()
-}
-
-fun generateRandomColor(): Int {
-    val rnd = Random()
-    return Color.argb(
-        Constants.ALPHA_COLOR_VALUE,
-        rnd.nextInt(Constants.BOUND_COLOR_VALUE),
-        rnd.nextInt(Constants.BOUND_COLOR_VALUE),
-        rnd.nextInt(Constants.BOUND_COLOR_VALUE)
-    )
 }

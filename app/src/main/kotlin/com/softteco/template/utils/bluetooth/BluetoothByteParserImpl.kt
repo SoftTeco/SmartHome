674a5612dd2445e9659a4d5003428c4d25975916
AlexJustUser <swimmersuper16@gmail.com>
2024-02-06 17:14:16 +0300

    refactor: refactoring

--- app/src/main/kotlin/com/softteco/template/utils/bluetooth/BluetoothByteParserImpl.kt
+++ app/src/main/kotlin/com/softteco/template/utils/bluetooth/BluetoothByteParserImpl.kt
@@ -1,7 +1,6 @@
-package com.softteco.template.utils
+package com.softteco.template.utils.bluetooth
 
 import com.softteco.template.Constants
-import com.softteco.template.Constants.BIT_SHIFT_VALUE
 import com.softteco.template.data.bluetooth.BluetoothByteParser
 import com.softteco.template.data.bluetooth.entity.BluetoothDeviceData
 import com.softteco.template.data.bluetooth.entity.BluetoothDeviceType
@@ -33,19 +32,5 @@ internal class BluetoothByteParserImpl @Inject constructor() : BluetoothBytePars
                 BluetoothDeviceData.DataLYWSD03MMC() // Change to any new devices
             }
         }
-    }
-
-    @OptIn(ExperimentalUnsignedTypes::class)
-    private fun characteristicByteConversation(
-        bytes: ByteArray,
-        startIndex: Int,
-        endIndex: Int
-    ): Double {
-        val array = bytes.copyOfRange(startIndex, endIndex).toUByteArray()
-        var result = 0
-        for (i in array.indices) {
-            result = result or (array[i].toInt() shl BIT_SHIFT_VALUE * i)
-        }
-        return result.toDouble()
     }
 }
